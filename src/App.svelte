a5572db2adab129a0bdee88925711d13db9469b9
Victor Cortés <cortes.victorhugo@gmail.com>
2020-09-26 23:00:13 -0500

    styles
    Global styles are identified with `:global()`
    Set global CSS variables
    `:global(:root)`

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,39 @@
 <script>
 	export let name;
 	export let lastName;
+	let svelteLogo = "https://cdn.svgporn.com/logos/svelte.svg"
 </script>
 
 <main>
 	<h1>Hello {name} {lastName}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Frontend Developer</p>
+	<img src={svelteLogo} alt="Svelte" />
 </main>
 
 <style>
+	:global(body) {
+		background-color: #f2eee2;
+		color:#0084f6;
+	}
+
+	:global(:root) {
+		--theme-color:purple;
+	}
+
+	p {
+		color: var(--theme-color);
+		font-size:22px;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+
+	main img {
+		width: 30%;
 	}
 
 	h1 {
